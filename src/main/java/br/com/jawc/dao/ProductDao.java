578508d751326d09b219ad292366f306d13d42c7
jawc-05 <joaoalfredowc@gmail.com>
2026-04-20 22:56:37 +0200

    feat: adding the save method

--- src/main/java/br/com/jawc/dao/ProductDao.java
+++ src/main/java/br/com/jawc/dao/ProductDao.java
@@ -4,10 +4,25 @@
 package br.com.jawc.dao;
 
 import br.com.jawc.domain.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class ProductDao implements IProductDao {
     @Override
     public Product save(Product product) {
-        return null;
+            EntityManagerFactory entityManagerFactory =
+                    Persistence.createEntityManagerFactory("ExampleJPA");
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+            entityManager.getTransaction().begin();
+            entityManager.persist(product);
+            entityManager.getTransaction().commit();
+
+            entityManager.close();
+            entityManagerFactory.close();
+
+
+            return product;
     }
 }
