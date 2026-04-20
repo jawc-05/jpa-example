2d62fbf0856a380ca6a22c26c9fad59dcaf4ef8b
jawc-05 <joaoalfredowc@gmail.com>
2026-04-20 22:24:19 +0200

    test: save test for course

--- src/main/java/br/com/jawc/dao/CourseDao.java
+++ src/main/java/br/com/jawc/dao/CourseDao.java
@@ -4,9 +4,26 @@
 package br.com.jawc.dao;
 
 import br.com.jawc.domain.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class CourseDao implements ICourseDao {
     @Override
     public Course save(Course course) {
+
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("ExampleJPA");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(course);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+
+        return course;
     }
 }
